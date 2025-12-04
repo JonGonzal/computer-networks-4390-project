6381f64b47fde592f66b4bbb28ebe5737154e39d
JonGonzal <12769872+JonGonzal@users.noreply.github.com>
2025-12-04 17:54:08 -0600

    feat, implemented main controller

--- server/controllers/main_controller.py
+++ server/controllers/main_controller.py
@@ -7,13 +7,13 @@ def handle_reqs(raw_reqs, ip):
     try:
         header_split, body = raw_reqs.split('\r\n\r\n', 1)
     except ValueError:
-        return "HTTTP/1.0 400 Bad Request\r\n\r\n".encode()
+        return "HTTP/1.0 400 Bad Request\r\n\r\n".encode()
     
     lines = header_split.split('\r\n')
     request_lines = lines[0].split()
 
     if len(request_lines) < 2:
-        return "HTTTP/1.0 400 Bad Request\r\n\r\n".encode()
+        return "HTTP/1.0 400 Bad Request\r\n\r\n".encode()
     
     method, path = request_lines[0], request_lines[1]
 
@@ -29,13 +29,35 @@ def handle_reqs(raw_reqs, ip):
     if fileName == "":
         fileName = "index.html"
 
-    fileName = os.path.join(settings.UPLOAD_DIRECTORY, fileName)
+    filePath = os.path.join(settings.UPLOAD_DIRECTORY, fileName)
 
     # GET and HEAD 
 
     if method in ['GET', 'HEAD']:
-        if os.path.exists(fileName) and os.path.isfile(fileName)
+        if os.path.exists(fileName) and os.path.isfile(fileName):
+            with open(fileName, 'rb') as f:
+                content = f.read()
 
+            response = "HTTP/1.0 200 OK\r\n"
+            response = f"Content-Length: {len(content)}\r\n\r\n"
 
+            if method = 'GET':
+                return response.encode() + content
+            else:
+                return response.encode()
+        else:
+            return "HTTP/1.0 404 NOT FOUND\r\n\r\n".encode()
 
+    elif method in ['POST', 'PUT']:
+        try:
+            if not os.path.exists(settings.UPLOAD_DIRECTORY):
+                os.makedirs(settings.UPLOAD_DIRECTORY)
 
+            with open(filePath, 'wb') as f:
+                f.write(body.encode('utf-8'))
+
+            return "HTTP/1.0 200 OK\r\n\r\n File Uploaded!".encode()
+        except Exception as e:
+            return f"HTTP/1.0 500 Internal ERROR\r\n\r\n{str(e)}".encode()
+
+    return f"HTTP/1.0 501 NOT Implemented\r\n\r\n".encode()
